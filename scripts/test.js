b024b03bdfb6a7b46f23dd3ec5dec593252c5e1e
sarmelano <sarmelano.main@gmail.com>
2023-10-20 21:46:52 -0700

    v2

--- scripts/test.js
+++ scripts/test.js
@@ -9,12 +9,6 @@ class Person {
 }
 
 class Apartment {
-  /**
-   * 
-   * @param {number} number 
-   * @param {number} roomsAmount 
-   * @param {Person[]} residents 
-   */
   constructor(number, roomsAmount, residents) {
     this.number = number;
     this.roomsAmount = roomsAmount;
@@ -33,12 +27,6 @@ class Apartment {
 }
 
 class House {
-  /**
-   * 
-   * @param {string} address 
-   * @param {number} floors 
-   * @param {Apartment[]} apartments 
-   */
   constructor(address, floors, apartments) {
     this.address = address;
     this.floors = floors;
@@ -56,118 +44,82 @@ class House {
   }
 }
 
-const form = document.createElement('form');
-form.id = 'form1';
-form.className = 'house-form';
+function createForm(id, className) {
+  const form = document.createElement('form');
+  form.id = id;
+  form.className = className;
+  return form;
+}
 
-const fields = [
+function addFieldsToForm(form, fields) {
+  fields.forEach(field => {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+
+    const label = document.createElement('label');
+    label.textContent = field.label;
+    label.setAttribute('for', field.id);
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = field.id;
+    input.setAttribute('autocomplete', 'off');
+
+    if (field.id.startsWith('number')) {
+      input.classList.add('shortest_input');
+    }
+
+    wrapper.appendChild(label);
+    wrapper.appendChild(input);
+    form.appendChild(wrapper);
+  });
+}
+
+function addButtonToForm(form, id, value, eventListener) {
+  const buttonWrapper = document.createElement('div');
+  buttonWrapper.className = 'wrapper';
+
+  const button = document.createElement('input');
+  button.type = 'button';
+  button.id = id;
+  button.value = value;
+  button.addEventListener('click', eventListener);
+
+  buttonWrapper.appendChild(button);
+  form.appendChild(buttonWrapper);
+}
+
+function getInputValue(id) {
+  return document.getElementById(id).value;
+}
+
+const form1 = createForm('form1', 'house-form');
+const fields1 = [
   { label: 'Адрес дома:', id: 'address' },
   { label: 'Кол-во этажей:', id: 'floors' },
   { label: 'Кол-во квартир:', id: 'apartments' }
 ];
+addFieldsToForm(form1, fields1);
+addButtonToForm(form1, 'house-form_btn', 'Далее', function () {
+  const address = getInputValue('address');
+  const floors = parseInt(getInputValue('floors'));
+  const apartmentsCount = parseInt(getInputValue('apartments'));
 
-fields.forEach(field => {
-  const wrapper = document.createElement('div');
-  wrapper.className = 'wrapper';
+  form1.classList.add('hidden');
 
-  const label = document.createElement('label');
-  label.textContent = field.label;
-  label.setAttribute('for', field.id); // Associate the label with the form field
+  const form2 = createForm('form2', 'appartment-form');
+  const fields2 = Array.from({ length: apartmentsCount }, (_, i) => [
+    { label: 'Номер ' + (i + 1) + ' квартиры:', id: `number${i + 1}` },
+    { label: 'Кол-во комнат:', id: `roomsAmount${i + 1}` },
+    { label: 'Кол-во жильцов:', id: `residents${i + 1}` }
+  ]).flat();
+  addFieldsToForm(form2, fields2);
+  addButtonToForm(form2, 'apartment-form_btn', 'Далее', function () {
+    const residentsCounts = Array.from({ length: apartmentsCount }, (_, i) => parseInt(getInputValue(`residents${i + 1}`)));
 
-  const input = document.createElement('input');
-  input.type = 'text';
-  input.id = field.id;
-  input.setAttribute('autocomplete', 'off'); // Add the autocomplete attribute
+    form2.classList.add('hidden');
 
-  wrapper.appendChild(label);
-  wrapper.appendChild(input);
-  form.appendChild(wrapper);
-});
-
-const buttonWrapper = document.createElement('div');
-buttonWrapper.className = 'wrapper';
-
-const form1btn = document.createElement('input');
-form1btn.type = 'button';
-form1btn.id = 'house-form_btn';
-form1btn.value = 'Далее';
-
-buttonWrapper.appendChild(form1btn);
-form.appendChild(buttonWrapper);
-
-document.body.appendChild(form);
-
-const submitButton = document.getElementById('house-form_btn');
-
-submitButton.addEventListener('click', function () {
-  const address = document.getElementById('address').value;
-  const floors = parseInt(document.getElementById('floors').value);
-  const apartments = parseInt(document.getElementById('apartments').value);
-
-  document.getElementById('form1').classList.add('hidden');
-
-  const form2 = document.createElement('form');
-  form2.id = 'form2';
-  form2.className = 'appartment-form';
-
-  const apartmentsCount = parseInt(apartments);
-
-  for (let i = 1; i <= apartmentsCount; i++) {
-    const wrapper = document.createElement('div');
-    wrapper.className = 'wrapper';
-
-    const fields = [
-      { label: 'Номер ' + i + ' квартиры:', id: `number${i}` },
-      { label: 'Кол-во комнат:', id: `roomsAmount${i}` },
-      { label: 'Кол-во жильцов:', id: `residents${i}` }
-    ];
-
-    fields.forEach(field => {
-      const label = document.createElement('label');
-      label.textContent = field.label;
-      label.setAttribute('for', field.id); // Associate the label with the form field
-
-      const input = document.createElement('input');
-      input.type = 'text';
-      input.id = field.id;
-
-      if (field.id.startsWith('number')) {
-        input.classList.add('shortest_input'); // Add the class here
-      }
-
-      wrapper.appendChild(label);
-      wrapper.appendChild(input);
-      form2.appendChild(wrapper);
-    });
-  }
-
-  const buttonWrapper2 = document.createElement('div');
-  buttonWrapper2.className = 'wrapper';
-
-  const form2btn = document.createElement('input');
-  form2btn.type = 'button';
-  form2btn.id = 'apartment-form_btn';
-  form2btn.value = 'Далее';
-
-  buttonWrapper2.appendChild(form2btn);
-  form2.appendChild(buttonWrapper2);
-
-  document.body.appendChild(form2);
-
-  form2btn.addEventListener('click', function () {
-    /* const numb = document.getElementById('number').value; */
-    const residentsCounts = [];
- 
-    for (let i = 1; i <= apartmentsCount; i++) {
-      const residentsCount = parseInt(document.getElementById(`residents${i}`).value);
-      residentsCounts.push(residentsCount);
-    }
-
-    document.getElementById('form2').classList.add('hidden');
-
-    const form3 = document.createElement('form');
-    form3.id = 'form3';
-
+    const form3 = createForm('form3', '');
     residentsCounts.forEach((residentsCount, i) => {
       const wrapper = document.createElement('div');
       wrapper.className = 'wrapper';
@@ -186,21 +138,7 @@ submitButton.addEventListener('click', function () {
 
       form3.appendChild(wrapper);
     });
-
-    const buttonWrapper3 = document.createElement('div');
-    buttonWrapper3.className = 'wrapper';
-
-    const form3btn = document.createElement('input');
-    form3btn.type = 'button';
-    form3btn.id = 'resident-form_btn';
-    form3btn.value = 'Готово';
-
-    buttonWrapper3.appendChild(form3btn);
-    form3.appendChild(buttonWrapper3);
-
-    document.body.appendChild(form3);
-
-    form3btn.addEventListener('click', function () {
+    addButtonToForm(form3, 'resident-form_btn', 'Готово', function () {
       const residents = [];
       const apartments = [];
 
@@ -208,20 +146,29 @@ submitButton.addEventListener('click', function () {
         const apartmentResidents = [];
 
         for (let j = 1; j <= residentsCount; j++) {
-          const residentName = document.getElementById(`resident${i + 1}_${j}`).value;
+          const residentName = getInputValue(`resident${i + 1}_${j}`);
           const person = new Person(residentName);
           apartmentResidents.push(person);
         }
 
-        const apartment = new Apartment(i + 1, parseInt(document.getElementById(`roomsAmount${i + 1}`).value), apartmentResidents);
+        const apartmentNumber = parseInt(getInputValue(`number${i + 1}`));
+        const roomsAmount = parseInt(getInputValue(`roomsAmount${i + 1}`));
+
+        const apartment = new Apartment(apartmentNumber, roomsAmount, apartmentResidents);
         apartments.push(apartment);
       });
 
       const house = new House(address, floors, apartments);
 
-      document.getElementById('form3').classList.add('hidden');
+      form3.classList.add('hidden');
 
       house.showInfo();
     });
+
+    document.body.appendChild(form3);
   });
+
+  document.body.appendChild(form2);
 });
+
+document.body.appendChild(form1);
